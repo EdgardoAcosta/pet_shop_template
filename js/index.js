f44010ef740f3367b929c885de2dabb12e3187e6
EdgardoAcosta <edgardo_acostaleal@hotmail.com>
2017-05-22 20:29:35 -0300

    carga de catalogo dinamicamente

---
+++ js/index.js
@@ -0,0 +1,172 @@
+/**
+ * Manuel Francisco Haro Arroyo: 10223004
+ * Edgardo Acosta Leal: 1022755
+ * José Richard Tejedo Vega: 10222991
+ * */
+
+
+function get_products() {
+
+    $.post("php/get_products.php", {'action': 'get'}, function (response) {
+    }, "json").done(function (response) {
+        if (response.success == "1") {
+            var id, name, description, type, price, stock, photo;
+            var cont = 0, aux = 0;
+            for (var i = 0; i < response['products'].length; i++) {
+
+                id = response['products'][i]['Id'];
+                console.log(id);
+                type = response['products'][i]['Type'];
+                name = response['products'][i]['Name'];
+                description = response['products'][i]['Description'];
+                price = response['products'][i]['Price'];
+                stock = response['products'][i]['Stock'];
+                photo = response['products'][i]['Photo'];
+
+                if (cont == 0) {
+                    $('#product-list').append('<div class="row products_list" id="row-prod' + aux + '" style="display: none">');
+                    $('#row-prod' + aux).append('<div class="col-lg-4 col-md-6" ' + type + '><div class="card">' +
+                        '<div class="view overlay hm-white-slight"> <img src="' + photo + '"' +
+                        ' alt="' + name + '" class="img-fluid"></div><div class="card-block">' +
+                        '<h4 class="card-title"><strong>' + name + '</strong></h4> <hr> ' +
+                        '<p class="card-text">' + description + '</p><div class="card-footer">' +
+                        '<span class="left">Stock: ' + stock + '</span> <span class="right">' +
+                        '<a href="#" data-original-title="Adicionar ao carrinho" type="button" data-toggle="tooltip" ' +
+                        'data-placement="top" title=""> <i class="fa fa-cart-plus"></i> </a>' +
+                        ' </span> </div> </div> </div> </div>');
+                }
+                else {
+                    $('#row-prod' + aux).append('<div class="col-lg-4 col-md-6 ' + type + '"><div class="card">' +
+                        '<div class="view overlay hm-white-slight"> <img src="' + photo + '"' +
+                        ' alt="' + name + '" class="img-fluid"></div><div class="card-block">' +
+                        '<h4 class="card-title"><strong>' + name + '</strong></h4> <hr> ' +
+                        '<p class="card-text">' + description + '</p><div class="card-footer">' +
+                        '<span class="left">Stock: ' + stock + '</span> <span class="right">' +
+                        '<a href="#" data-original-title="Adicionar ao carrinho" type="button" data-toggle="tooltip" ' +
+                        'data-placement="top" title=""> <i class="fa fa-cart-plus"></i> </a>' +
+                        ' </span> </div> </div> </div> </div>');
+                }
+                cont++;
+                if (cont >= 3) {
+                    $('#product-list').append('</div>');
+                    cont = 0;
+                    aux++;
+                }
+                /*
+                 <div class="row ">
+                 <div class="col-md-12">
+                 <div class="col-lg-4 col-md-6">
+                 <div class="card">
+                 <div class="view overlay hm-white-slight">
+                 <img src="Images/Categories/Accessories/collar perro.jpg" alt="Collar para cachorro"
+                 class="img-fluid">
+                 </div>
+
+                 <div class="card-block">
+                 <h4 class="card-title"><strong>Collar para cachorro</strong></h4>
+
+                 <hr>
+
+                 <p class="card-text">Descrição do collar para cachorro.</p>
+
+                 <div class="card-footer">
+                 <span class="left">Stock: 40</span>
+                 <span class="right">
+                 <a href="#" data-original-title="Adicionar ao carrinho" type="button"
+                 data-toggle="tooltip" data-placement="top" title="">
+                 <i class="fa fa-cart-plus"></i>
+                 </a>
+                 </span>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6">
+                 <div class="card">
+                 <div class="view overlay hm-white-slight">
+                 <img src="Images/Categories/Accessories/kennei-trans-.jpg" alt="Casa para cachorro"
+                 class="img-fluid">
+                 </div>
+
+                 <div class="card-block">
+                 <h4 class="card-title"><strong>Casa para cachorro</strong></h4>
+
+                 <hr>
+
+                 <p class="card-text">Descrição da casa para cachorro.</p>
+
+                 <div class="card-footer">
+                 <span class="left">Stock: 15</span>
+                 <span class="right">
+                 <a href="#" data-original-title="Adicionar ao carrinho" type="button"
+                 data-toggle="tooltip" data-placement="top" title="">
+                 <i class="fa fa-cart-plus"></i>
+                 </a>
+                 </span>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
+                 <div class="col-lg-4 col-md-6">
+                 <div class="card">
+                 <div class="view overlay hm-white-slight">
+                 <img src="Images/Categories/Accessories/plato para perro.jpg" alt="Prato para cachorro"
+                 class="img-fluid">
+                 </div>
+
+                 <div class="card-block">
+                 <h4 class="card-title"><strong>Prato para cachorro</strong></h4>
+
+                 <hr>
+
+                 <p class="card-text">Descrição do collar para cachorro.</p>
+
+                 <div class="card-footer">
+                 <span class="left">Stock: 40</span>
+                 <span class="right">
+                 <a href="#" data-original-title="Adicionar ao carrinho" type="button"
+                 data-toggle="tooltip" data-placement="top" title="">
+                 <i class="fa fa-cart-plus"></i>
+                 </a>
+                 </span>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+                 */
+            }
+            $('#row-prod0').show();
+            $('#product-list').append('<div class="text-xs-center">' +
+                '<button type="button" class="btn light-blue darken-4" onclick="show_more();" id="show-more">Ver mais</button>' +
+                '<button type="button" class="btn light-blue darken-4" onclick="show_less();" id="show-less" style="display: none">Ver menos</button></div>');
+
+        }
+        else {
+            /*Error*/
+        }
+
+    });
+}
+function show_more() {
+    var show = document.getElementsByClassName('products_list'), i;
+    for (var i = 0; i < show.length; i ++) {
+        show[i].style.display = 'block';
+    }
+    document.getElementById('show-more').style.display = 'none';
+    document.getElementById('show-less').style.display = 'block';
+}
+function show_less() {
+    var show = document.getElementsByClassName('products_list'), i;
+    for (var i = 1; i < show.length; i ++) {
+        show[i].style.display = 'none';
+    }
+}
+
+$(document).ready(function () {
+    get_products();
+
+});
