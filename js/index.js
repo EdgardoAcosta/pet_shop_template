ebb145822255705a9e5ed26c4bb6cf10a3057b02
MaNuEl33 <manuelharo1994@gmail.com>
2017-06-10 19:13:41 -0300

    Metodo listar productos para administrador creado

--- js/index.js
+++ js/index.js
@@ -587,6 +587,58 @@ function register_service() {
     });
 }
 
+function list_products() {
+    var open = indexedDB.open("pet_shop", 1);
+
+    open.onsuccess = function() {
+        var db = open.result;
+
+        var transaction = db.transaction("product", "readonly");
+        var objectStore = transaction.objectStore("product");
+
+        transaction.oncomplete = (e) => {
+            $(".del_product").click(function() {
+                var id = $(this).data("id");
+                var request = db.transaction(["product"], "readwrite").objectStore("product").delete(id);
+                request.onsuccess = (e) => window.location.href = "admin_products.html";
+            });
+        }
+
+        objectStore.openCursor().onsuccess = (e) => {
+            var cursor = e.target.result;
+            var element = $("#produtos");
+            if (cursor) {
+                var id = cursor.key;
+                var name = cursor.value.Name;
+                var description = cursor.value.Description;
+                var price = cursor.value.Price;
+                var stock = cursor.value.Stock;
+                var photo = cursor.value.Photo;
+                var type = cursor.value.Type;
+
+                element.append(
+                '<div class="col-lg-4">' +
+                    '<div class="card">' +
+                        '<img class="img-fluid" src="' + photo + '">' +
+                        '<div class="card-block">' +
+                            '<h4 class="card-title"><strong>' + name + '</strong></h4>' +
+                            '<p class="card-text">' + description.substring(0, 50) + '...</p>' +
+                            '<a href="detail_product.html?id=' + id + '" class="black-text"><h5>Ler mais <i class="fa fa-chevron-right"></i></h5></a>' +
+                            '<div class="offset-lg-1">' +
+                                '<a href="edit_product.html?id=' + id + '" target="_blank" class="btn btn-primary">Editar</a>' +
+                                '<button class="btn btn-danger del_product" data-id="' + id +'">Remover</button>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>'
+                );
+
+                cursor.continue();
+            }
+        }
+    }
+}
+
 
 $(document).ready(function () {
     toastr.options = {
@@ -620,7 +672,7 @@ $(document).ready(function () {
     register_client();
     register_product();
     register_service();
-	
+    list_products();	
     $('.mdb-select').material_select();
 //Disable adding more than 1 item to cart, in cart can be change the number of items
     $(document).on('click', "a.addcart", function () {
