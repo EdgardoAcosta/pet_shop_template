447b64e995017a29b1c80547d3b1a4497b81d53f
MaNuEl33 <manuelharo1994@gmail.com>
2017-06-10 23:48:34 -0300

    Create register_service.js

---
+++ js/register_service.js
@@ -0,0 +1,26 @@
+var db;
+
+$(document).ready(function() {
+	var openRequest = indexedDB.open("pet_shop", 1);
+	openRequest.onsuccess = (e) => {
+		db = e.target.result;
+	}
+
+	$("#reg_service").click(function() {
+		var dataArray = $("#register_service").serializeArray();
+        var data = {};
+
+        $(dataArray).each(function(i, field) {
+            data[field.name] = field.value;
+        });
+
+        var transaction = db.transaction(["service"], "readwrite");
+
+        var objectStore = transaction.objectStore("service");
+        var request = objectStore.add({ Id: data['id'], Name: data['nome'], Description: data['descricao'], Price: data['preco'], Photo: data['photo']});
+
+        request.onsuccess = (e) => {
+            alert("Servico cadastrado");
+        }
+    });
+});
